0e9426f3f00e82fdf432df9eec8401a5ce6fc568
Sergio <slorentemontoro@gmail.com>
2021-11-30 13:06:55 +0100

    Cambio de progessbar

--- app/src/main/java/com/example/carrace/Car.kt
+++ app/src/main/java/com/example/carrace/Car.kt
@@ -1,6 +1,6 @@
 package com.example.carrace
 
-class Cars (val nombre:String, val velocidadMaxima:Int,val aceleracion:Int,val desaceleracion:Int,val gasolina:Int,val design:Int,val fondo:Int) {
+class Cars (val nombre:String, val velocidadMaxima:Int,val aceleracion:Int,val desaceleracion:Int,val gasolina:Int,val design:Int) {
 
 
 
--- app/src/main/java/com/example/carrace/SliderCar.kt
+++ app/src/main/java/com/example/carrace/SliderCar.kt
@@ -1,11 +1,11 @@
 package com.example.carrace
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import pl.droidsonroids.gif.GifImageView
 
 class SliderCar : AppCompatActivity() {
@@ -13,69 +13,71 @@ class SliderCar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slider_car)
 
-        val ArrayCoches= mutableListOf<Cars>();
-        var fondo_coche=findViewById<ImageView>(R.id.paisaje)
-        var gifcoche=findViewById<GifImageView>(R.id.coches1)
-        var aceleracion=findViewById<ProgressBar>(R.id.accelerate)
-        var velocidadMaxima=findViewById<ProgressBar>(R.id.maxspeed)
-        var desaceleracion=findViewById<ProgressBar>(R.id.decelerate)
-        var gasolina=findViewById<ProgressBar>(R.id.fuel)
-        var sw_acc=findViewById<TextView>(R.id.acceleratevalue)
-        var sw_vmax=findViewById<TextView>(R.id.maxspeedvalue)
-        var sw_desc=findViewById<TextView>(R.id.deceleratevalue)
-        var sw_gas=findViewById<TextView>(R.id.fuelvalue)
-        var next_car=findViewById<ImageButton>(R.id.arrow_post)
-        var previous_car=findViewById<ImageButton>(R.id.arrow_ant)
-        fun mostrarcoche(coche:Cars){
-            fondo_coche.setImageResource(coche.fondo)
-            sw_acc.setText(coche.aceleracion.toString())
-            sw_desc.setText(coche.desaceleracion.toString())
-            sw_gas.setText(coche.gasolina.toString())
-            sw_vmax.setText(coche.velocidadMaxima.toString())
-            gifcoche.setImageResource(coche.design)
-            velocidadMaxima.setProgress(coche.velocidadMaxima)
-            aceleracion.setProgress(coche.aceleracion)
-            desaceleracion.setProgress(coche.desaceleracion)
-            gasolina.setProgress(coche.gasolina)
+        val arrayCar = mutableListOf<Cars>()
+        var carGif = findViewById<GifImageView>(R.id.coches1)
+        var acceleration = findViewById<ProgressBar>(R.id.accelerate)
+        var maxSpeed = findViewById<ProgressBar>(R.id.maxspeed)
+        var deceleration = findViewById<ProgressBar>(R.id.decelerate)
+        var fuel = findViewById<ProgressBar>(R.id.fuel)
+        var sw_acc = findViewById<TextView>(R.id.acceleratevalue)
+        var sw_maxspeed = findViewById<TextView>(R.id.maxspeedvalue)
+        var sw_dece = findViewById<TextView>(R.id.deceleratevalue)
+        var sw_fuel = findViewById<TextView>(R.id.fuelvalue)
+        var next_car = findViewById<ImageButton>(R.id.arrow_post)
+        var previous_car = findViewById<ImageButton>(R.id.arrow_ant)
+
+
+        fun mostrarcoche(coche: Cars) {
+            carGif.setImageResource(coche.design)
+            acceleration.progress = coche.aceleracion
+            maxSpeed.progress = coche.velocidadMaxima
+            deceleration.progress = coche.desaceleracion
+            fuel.progress = coche.gasolina
+            sw_acc.text = coche.aceleracion.toString()
+            sw_dece.text = coche.desaceleracion.toString()
+            sw_fuel.text = coche.gasolina.toString()
+            sw_maxspeed.text = coche.velocidadMaxima.toString()
         }
 
-        var posicion :Int=0
-        val coche1=Cars("octane",70,6,3,90,R.drawable.coche1,R.drawable.fondo1)
-        val coche2=Cars("octane V2",80,5,6,75,R.drawable.coche2,R.drawable.fondo2)
-        val coche3=Cars("lorean",100,8,6,60,R.drawable.coche3,R.drawable.fondo3)
-        val coche4=Cars("bearch",85,10,5,75,R.drawable.coche4,R.drawable.fondo4)
-        val coche5=Cars("Rayo Mcqueen",95,12,8,50,R.drawable.coche5,R.drawable.fondo5)
-        ArrayCoches.add(coche1)
-        ArrayCoches.add(coche2)
-        ArrayCoches.add(coche3)
-        ArrayCoches.add(coche4)
-        ArrayCoches.add(coche5)
 
 
-        aceleracion.max=30
-        velocidadMaxima.max=100
-        desaceleracion.max=30
-        gasolina.max=100
-        mostrarcoche(ArrayCoches[posicion])
+        var posicion: Int = 0
+        val BowserCar = Cars("Bowser", 100, 4, 10, 90, R.drawable.coche1)
+        val PeachCar = Cars("Peach", 80, 10, 4, 70, R.drawable.coche2)
+        val DonkeyCar = Cars("Donkey Kong", 100, 20, 10, 100, R.drawable.coche3)
+        val MarioCar = Cars("Mario", 80, 10, 10, 60, R.drawable.coche4)
+        val LuigiCar = Cars("Luigi", 95, 12, 8, 50, R.drawable.coche5)
+        arrayCar.add(BowserCar)
+        arrayCar.add(PeachCar)
+        arrayCar.add(DonkeyCar)
+        arrayCar.add(MarioCar)
+        arrayCar.add(LuigiCar)
+
+
+        acceleration.max = 30
+        maxSpeed.max = 100
+        deceleration.max = 30
+        fuel.max = 100
+        mostrarcoche(arrayCar[posicion])
 
 
 
         next_car.setOnClickListener {
-            posicion++;
-            if (posicion<ArrayCoches.size){
-                mostrarcoche(ArrayCoches[posicion])
-            }else{
-                posicion=0;
-                mostrarcoche(ArrayCoches[posicion])
+            posicion++
+            if (posicion < arrayCar.size) {
+                mostrarcoche(arrayCar[posicion])
+            } else {
+                posicion = 0
+                mostrarcoche(arrayCar[posicion])
             }
         }
         previous_car.setOnClickListener {
-            posicion--;
-            if(posicion<0){
-                posicion=ArrayCoches.size-1
-                mostrarcoche(ArrayCoches[posicion])
-            }else{
-                mostrarcoche(ArrayCoches[posicion])
+            posicion--
+            if (posicion < 0) {
+                posicion = arrayCar.size - 1
+                mostrarcoche(arrayCar[posicion])
+            } else {
+                mostrarcoche(arrayCar[posicion])
             }
         }
     }
