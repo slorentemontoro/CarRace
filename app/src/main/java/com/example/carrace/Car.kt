76373d5856d8630ed01f096b800d44f81f5212e5
Sergio Lorente <slorentemontoro@gmail.com>
2021-12-07 12:12:47 +0100

    OK lets go

--- app/src/main/java/com/example/carrace/Car.kt
+++ app/src/main/java/com/example/carrace/Car.kt
@@ -2,7 +2,7 @@ package com.example.carrace
 
 import kotlin.random.Random
 
-class Cars (val name:String,var speed: Int, var maxSpeed:Int,val accelerate:Int,val stability:Int, val Image:Int, var Distance: Int) {
+class Cars (var name:String, var speed: Int, var maxSpeed:Int, val accelerate:Int, val stability:Int, var Image:Int, var Distance: Int) {
 
 
 
@@ -17,20 +17,20 @@ class Cars (val name:String,var speed: Int, var maxSpeed:Int,val accelerate:Int,
 
     fun move(){
         Distance += speed
+
     }
 
     fun overturn(){
-        var random = Random.nextInt(1, 6)
+        var random = Random.nextInt(0, 11)
         if (random == stability){
             Distance = 0
             speed = 0
-
         }
     }
 
     fun boost(){
 
-        var random = Random.nextInt(1, 10)
+        var random = Random.nextInt(0, 11)
         if (random == 5){
             speed += 10
         }
--- app/src/main/java/com/example/carrace/RaceStart.kt
+++ app/src/main/java/com/example/carrace/RaceStart.kt
@@ -1,28 +1,31 @@
 package com.example.carrace
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import pl.droidsonroids.gif.GifImageView
-import kotlin.random.Random
+import java.util.*
+import android.os.Looper
+
+
+
 
 class RaceStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_race)
 
-        var carSelectedName = carPlayerName
         var Cars = raceCars
         var Circuit = raceCircuit
-        var winner = findViewById<TextView>(R.id.winner)
-        var winnerImage = findViewById<GifImageView>(R.id.winnerImage)
-
         var Bowser = findViewById<SeekBar>(R.id.Bowser)
         var Peach = findViewById<SeekBar>(R.id.Peach)
         var DK = findViewById<SeekBar>(R.id.DK)
         var Mario = findViewById<SeekBar>(R.id.Mario)
         var Luigi = findViewById<SeekBar>(R.id.Luigi)
+        var winnertext = findViewById<TextView>(R.id.winner)
+        var winnerImage = findViewById<ImageView>(R.id.winnerImage)
 
 
         fun setStyle(){
@@ -31,37 +34,50 @@ class RaceStart : AppCompatActivity() {
             DK.max = Circuit.Distance
             Mario.max = Circuit.Distance
             Luigi.max = Circuit.Distance
-
-
-            Bowser.progress = bowserCar.Distance
-            Peach.progress = peachCar.Distance
-            DK.progress = donkeyCar.Distance
-            Mario.progress = marioCar.Distance
-            Luigi.progress = luigiCar.Distance
         }
 
-        var continuar = true;
 
+        var continuar = true
+
+
+
+        Thread(Runnable {
         while (continuar == true){
             setStyle()
             Cars.forEach{
+
+                print(it.name + "\n")
                 it.accelerate()
                 it.move()
                 it.overturn()
                 it.boost()
+                println(it.Distance)
+
+
+
+                Bowser.progress = bowserCar.Distance
+                Peach.progress = peachCar.Distance
+                DK.progress = donkeyCar.Distance
+                Mario.progress = marioCar.Distance
+                Luigi.progress = luigiCar.Distance
+                Thread.sleep(100)
+
 
                 if (it.Distance >= Circuit.Distance){
                     continuar = false
-                    if (it.name == carSelectedName){
-                        winner.text = "You are the winner. Congratulations!!!"
-                        winnerImage.setImageResource(it.Image)
-                    }else{
-                        winner.text = "The Winner is "+ it.name+ ". Good luck next time :)"
-                        winnerImage.setImageResource(it.Image)
-                    }
 
+
+                    if (it.name == carPlayerName) {
+                        winnertext.text = "You are the winner. Congratulations!!!"
+                    } else {
+                        winnertext.text = "The Winner is " + it.name + ". Good luck next time :)"
+                    }
+                    winnerImage.setImageResource(it.Image)
                 }
             }
         }
+
+
+        }).start()
     }
 }
